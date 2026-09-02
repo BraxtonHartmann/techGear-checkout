c70bac5a342185e80cff5bca343ef492d6cc0bc9
Braxton Hartmann <braxtonhartmann@mail.weber.edu>
2026-09-02 09:13:30 -0600

    initial commit

---
+++ Cyberpunk-Gear-Shop/src/App.jsx
@@ -0,0 +1,30 @@
+function App(){
+  const shopInventory = [
+    { 
+      id: "g1", 
+      name: "Neural Link Processor", 
+      category: "Cyberware", 
+      description: "Boosts reaction time and memory bandwidth.", 
+      basePrice: 1200 
+    },
+    { 
+      id: "g2", 
+      name: "Thermal Katana", 
+      category: "Weapons", 
+      description: "Plasma-heated blade for cutting armor.", 
+      basePrice: 850 
+    },
+    { 
+      id: "g3", 
+      name: "Optical Camo Cloak", 
+      category: "Apparel", 
+      description: "Renders user semi-invisible to security sensors.", 
+      basePrice: 2100 
+    }
+  ];
+
+
+
+}
+
+export default App;
